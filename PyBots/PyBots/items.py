b4298b2c9e46026306a1574b032fc156e79e3303
p9s <mc.cheung@aol.com>
2017-04-20 14:30:22 +0800

    item data

--- PyBots/PyBots/items.py
+++ PyBots/PyBots/items.py
@@ -11,4 +11,34 @@ import scrapy
 class PybotsItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    pass
+    
+    title = scrapy.Field()
+    url = scrapy.Field()
+    
+    # http://coding.net =>  coding_net_123  其中 123 是每个 item 的 id 
+    uniq_id = scrapy.Field()
+   
+    # 计算之后的发布日期
+    date = scrapy.Field()
+
+    # 3 天之前, 之类的
+    date_str = scrapy.Field()
+    
+    # 价格 
+    price = scrapy.Field()
+   
+    # 阅读数, 报名数之类
+    reads = scrapy.Field()
+    
+    # 项目图片
+    cover = scrapy.Field()
+
+    # 语言, 时间限制等 , 数组
+    tags = scrapy.Field()
+
+    # 项目的详细说明 
+    content = scrapy.Field()
+
+    # 项目发布人或公司简介
+    company = scrapy.Field()
+
