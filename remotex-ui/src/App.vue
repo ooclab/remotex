ed2e72e74428908ed5c466afe755a210dcc5740b
Jack <yiping.luoyp@gmail.com>
2017-04-24 12:06:38 +0800

    finish basic functions

--- remotex-ui/src/App.vue
+++ remotex-ui/src/App.vue
@@ -53,79 +53,83 @@
 
     <!-- part 2 搜索栏 -->
     <div>
-      <mu-text-field style="width:60%;" hintText="输入关键词"/>
-      <mu-flat-button label="搜索" class="demo-flat-button" icon="search" primary/>
+
+      <!-- 关键词输入框 -->
+      <mu-text-field style="width:60%;" label="请输入关键词" labelFloat @change="textChanged"/>
+
+      <!-- 搜索按钮 -->
+      <mu-flat-button label="搜索" class="demo-flat-button" icon="search" primary @click="search"/>
     </div>
 
     <!-- part 3 类目栏 -->
     <div>
-      <mu-raised-button style="width:40%; margin: 5px 5px 5px 0;" @click="openBottomSheet1" label="平台" icon="assignment_turned_in"/>
+      <mu-raised-button style="width:40%; margin: 5px 5px 5px 0;" @click="openBottomSheet1" :label="platformTitle" icon="assignment_turned_in"/>
 
       <!-- 平台 -->
       <mu-bottom-sheet :open="bottomSheet1" @close="closeBottomSheet1">
-        <mu-list @itemClick="closeBottomSheet1" style="height:200px">
+        <mu-list @itemClick="closeBottomSheet1()" style="height:200px">
           <mu-sub-header>
             请选择平台
           </mu-sub-header>
-          <mu-list-item title="全部"/>
-          <mu-list-item title="外包众包"/>
-          <mu-list-item title="远程工作"/>
+          <mu-list-item title="全部" @click="catagoryItemClick('platform', '0', '平台')"/>
+          <mu-list-item title="外包众包" @click="catagoryItemClick('platform', '1', '外包众包')"/>
+          <mu-list-item title="远程工作" @click="catagoryItemClick('platform', '2', '远程工作')"/>
         </mu-list>
       </mu-bottom-sheet>
 
       <!-- 角色 -->
-      <mu-raised-button style="width:40%; margin: 5px 0 5px 5px;" @click="openBottomSheet2" label="角色" icon="build"/>
+      <mu-raised-button style="width:40%; margin: 5px 0 5px 5px;" @click="openBottomSheet2" :label="roleTitle" icon="build"/>
       <mu-bottom-sheet :open="bottomSheet2" @close="closeBottomSheet2">
         <mu-list @itemClick="closeBottomSheet2" style="height:240px">
           <mu-sub-header>
             请选择角色
           </mu-sub-header>
-          <mu-list-item title="全部"/>
-          <mu-list-item title="项目管理"/>
-          <mu-list-item title="产品经理"/>
-          <mu-list-item title="设计"/>
-          <mu-list-item title="开发"/>
-          <mu-list-item title="测试"/>
-          <mu-list-item title="运维"/>
-          <mu-list-item title="市场"/>
-          <mu-list-item title="运营"/>
+          <mu-list-item title="全部" @click="catagoryItemClick('role', '0', '角色')"/>
+          <mu-list-item title="项目管理" @click="catagoryItemClick('role', '1', '项目管理')""/>
+          <mu-list-item title="产品经理" @click="catagoryItemClick('role', '2', '产品经理')"/>
+          <mu-list-item title="设计" @click="catagoryItemClick('role', '3', '设计')"/>
+          <mu-list-item title="开发" @click="catagoryItemClick('role', '4', '开发')"/>
+          <mu-list-item title="测试" @click="catagoryItemClick('role', '5', '测试')"/>
+          <mu-list-item title="运维" @click="catagoryItemClick('role', '6', '运维')"/>
+          <mu-list-item title="市场" @click="catagoryItemClick('role', '7', '市场')"/>
+          <mu-list-item title="运营" @click="catagoryItemClick('role', '8', '运营')"/>
         </mu-list>
       </mu-bottom-sheet>
 
       <!-- 工作 -->
-      <mu-raised-button style="width:40%; margin: 5px 5px 5px 0;" @click="openBottomSheet3" label="工作" icon="devices"/>
+      <mu-raised-button style="width:40%; margin: 5px 5px 5px 0;" @click="openBottomSheet3" :label="workTitle" icon="devices"/>
       <mu-bottom-sheet :open="bottomSheet3" @close="closeBottomSheet3">
         <mu-list @itemClick="closeBottomSheet3" style="height:240px">
           <mu-sub-header>
             请选择工作
           </mu-sub-header>
-          <mu-list-item title="全部"/>
-          <mu-list-item title="Web 页面"/>
-          <mu-list-item title="App 移动应用"/>
-          <mu-list-item title="微信开发"/>
-          <mu-list-item title="咨询"/>
+          <mu-list-item title="全部" @click="catagoryItemClick('work', '0', '工作')"/>
+          <mu-list-item title="Web 页面" @click="catagoryItemClick('work', '1', 'Web 页面')"/>
+          <mu-list-item title="App 移动应用" @click="catagoryItemClick('work', '2', 'App 移动应用')"/>
+          <mu-list-item title="微信开发" @click="catagoryItemClick('work', '3', '微信开发')"/>
+          <mu-list-item title="咨询" @click="catagoryItemClick('work', '4', '咨询')"/>
         </mu-list>
       </mu-bottom-sheet>
 
       <!-- 城市 -->
-      <mu-raised-button style="width:40%; margin: 5px 0 5px 5px;" @click="openBottomSheet4" label="城市" icon="location_city"/>
+      <mu-raised-button style="width:40%; margin: 5px 0 5px 5px;" @click="openBottomSheet4" :label="cityTitle" icon="location_city"/>
       <mu-bottom-sheet :open="bottomSheet4" @close="closeBottomSheet4">
         <mu-list @itemClick="closeBottomSheet4" style="height:240px">
           <mu-sub-header>
             请选择城市
           </mu-sub-header>
-          <mu-list-item title="全部"/>
-          <mu-list-item title="北京"/>
-          <mu-list-item title="上海"/>
-          <mu-list-item title="广州"/>
-          <mu-list-item title="深圳"/>
-          <mu-list-item title="杭州"/>
-          <mu-list-item title="成都"/>
-          <mu-list-item title="武汉"/>
-          <mu-list-item title="南京"/>
-          <mu-list-item title="西安"/>
-          <mu-list-item title="厦门"/>
-          <mu-list-item title="其他"/>
+          <mu-list-item title="全部" @click="catagoryItemClick('city', '0', '城市')"/>
+          <mu-list-item title="北京" @click="catagoryItemClick('city', '1', '北京')"/>
+          <mu-list-item title="上海" @click="catagoryItemClick('city', '2', '上海')"/>
+          <mu-list-item title="广州" @click="catagoryItemClick('city', '3', '广州')"/>
+          <mu-list-item title="深圳" @click="catagoryItemClick('city', '4', '深圳')"/>
+          <mu-list-item title="杭州" @click="catagoryItemClick('city', '5', '杭州')"/>
+          <mu-list-item title="成都" @click="catagoryItemClick('city', '6', '成都')"/>
+          <mu-list-item title="武汉" @click="catagoryItemClick('city', '7', '武汉')"/>
+          <mu-list-item title="南京" @click="catagoryItemClick('city', '8', '南京')"/>
+          <mu-list-item title="西安" @click="catagoryItemClick('city', '9', '西安')"/>
+          <mu-list-item title="厦门" @click="catagoryItemClick('city', '10', '厦门')"/>
+          <mu-list-item title="其他" @click="catagoryItemClick('city', '11', '其他')"/>
         </mu-list>
       </mu-bottom-sheet>
     </div>
@@ -134,10 +138,9 @@
     <div>
       <!--<mu-tabs style="background:#424242;">-->
       <mu-tabs style="background:#ededed; margin-top: 10px;">
-        <mu-tab style="color: #616161;" value="tab1" title="热门机会"/>
-        <mu-tab style="color: #616161;" value="tab2" title="最新发布"/>
-        <!--<mu-tab style="color: #BDBDBD;" value="tab3" title="高薪报酬"/>-->
-        <mu-tab style="color: #F5193E;" value="tab3" title="高薪报酬"/>
+        <mu-tab :style="sortTab1Style" value="tab1" title="热门机会" @click="sortItemClick('view_count')"/>
+        <mu-tab :style="sortTab2Style" value="tab2" title="最新发布" @click="sortItemClick('release_date')"/>
+        <mu-tab :style="sortTab3Style" value="tab3" title="高薪报酬" @click="sortItemClick('price')"/>
       </mu-tabs>
     </div>
 
@@ -207,26 +210,54 @@ export default {
       bottomSheet2: false,
       bottomSheet3: false,
       bottomSheet4: false,
-      list: [],
+      list: [], // 数据
       num: 10, // 每页显示个数
       loading: false, // 加载中图标
       scroller: null, // 页面上可滚动的元素
       total: 0, // 列表数据总数
       openDrawer: false, // 是否打开右侧抽屉
-      docked: true //是否遮罩页面
+      docked: true, //是否遮罩页面
+      // 类目条件
+      platformItem: '',
+      roleItem: '',
+      workItem: '',
+      cityItem: '',
+      // 类目标题
+      platformTitle: '平台',
+      roleTitle: '角色',
+      workTitle: '工作',
+      cityTitle: '城市',
+      sort: '&sb=view_count&sd=desc', // 排序条件
+      // 排序按钮样式
+      sortActive: '',
+      sortDeactive: '',
+      sortTab1Style: '',
+      sortTab2Style: '',
+      sortTab3Style: '',
+      keywords: '', // 搜索框内的关键词
+      pageNum: 1,
+      endPage: false,
+      url: '' // 组装搜索的 URL
     }
   },
   beforeMount() {
-    Vue.http.get('https://remotex.ooclab.org/api/jobx/job').then(response => {
-      console.log(response.data.data)
+    Vue.http.get('https://remotex.ooclab.org/api/jobx/job?sb=view_count&sd=desc').then(response => {
       this.list = response.data.data
       this.total = response.data.total
     }, response => {
-      console.log('error')
+      // console.log('error')
     })
   },
   mounted () {
     this.scroller = this.$el
+  },
+  created () {
+    this.sortActive = 'color: #F5193E'
+    this.sortDeactive = 'color: #616161'
+    this.sortTab1Style = this.sortActive
+    this.sortTab2Style = this.sortDeactive
+    this.sortTab3Style = this.sortDeactive
+    this.url = 'https://remotex.ooclab.org/api/jobx/job?sb=view_count&sd=desc'
   },
   methods: {
     closeBottomSheet1 () {
@@ -253,21 +284,103 @@ export default {
     openBottomSheet4 () {
       this.bottomSheet4 = true
     },
-    // 加载列表数据
+    // 加载更多列表数据
     loadMore () {
-      this.loading = true
-      setTimeout(() => {
-        for (let i = this.num; i < this.num + 10; i++) {
-          this.list.push('item' + (i + 1))
-        }
-        this.num += 10
-        this.loading = false
-      }, 2000)
+      if (!this.endPage){
+        this.loading = true
+        this.pageNum = this.pageNum + 1
+        setTimeout(() => {
+          Vue.http.get(this.url + '&p=' + this.pageNum).then(response => {
+            console.log(response)
+            if (response.data.data.length == 0){
+              this.endPage = true
+            } else {
+              this.list.push(response.data.data)
+              this.endPage = false
+            }
+            this.total = response.data.total
+            this.loading = false
+          }, response => {
+            // console.log('error')
+            this.loading = false
+            this.endPage = true
+          })
+        }, 500)
+      }
     },
     // 打开、关闭抽屉
     toggleDrawer (flag) {
       this.openDrawer = !this.openDrawer
       this.docked = !flag
+    },
+    // 重新加载列表数据
+    search () {
+      this.list = []
+      this.loading = true
+      this.pageNum = 1
+      this.endPage = false
+      this.url = 'https://remotex.ooclab.org/api/jobx/job?' + this.platformItem + this.roleItem + this.workItem + this.cityItem + this.sort + this.keywords
+      Vue.http.get(this.url).then(response => {
+        console.log(response)
+        this.list = response.data.data
+        this.total = response.data.total
+        this.loading = false
+      }, response => {
+        // console.log('error')
+        this.loading = false
+      })
+    },
+    // 点击类目按钮
+    catagoryItemClick (lk, lv, title) {
+      if (lk == 'platform') {
+          this.platformItem = ''
+          this.platformTitle = title
+      } else if (lk == 'role') {
+        this.roleItem = ''
+        this.roleTitle = title
+      } else if (lk == 'work') {
+        this.workItem = ''
+        this.workTitle = title
+      } else if (lk == 'city') {
+        this.cityItem = ''
+        this.cityTitle = title
+      }
+      if (lv != '0') {
+        if (lk == 'platform') {
+          this.platformItem = '&' + lk + '=' + lv
+        } else if (lk == 'role') {
+          this.roleItem = '&' + lk + '=' + lv
+        } else if (lk == 'work') {
+          this.workItem = '&' + lk + '=' + lv
+        } else if (lk == 'city') {
+          this.cityItem = '&' + lk + '=' + lv
+        }
+      }
+      this.search()
+    },
+    // 点击排序按钮
+    sortItemClick (sb) {
+      this.sort = '&sb=' + sb + '&sd=desc'
+      this.sortTab1Style = this.sortDeactive
+      this.sortTab2Style = this.sortDeactive
+      this.sortTab3Style = this.sortDeactive
+      if (sb == 'view_count'){
+        this.sortTab1Style = this.sortActive
+      } else if (sb == 'release_date'){
+        this.sortTab2Style = this.sortActive
+      } else if (sb == 'price'){
+        this.sortTab3Style = this.sortActive
+      } 
+      this.search()
+    },
+    // 搜索框事件
+    textChanged (e) {
+      var textValue = e.target.value
+      if (textValue == '') {
+        this.keywords = ''
+      } else {
+        this.keywords = '&lk=title&lv=' + e.target.value
+      }
     }
   }
 }
